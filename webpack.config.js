18b12be387eef8b2ba4876aa4ea809520ddd5cdf
Alejandro Dotor <alejandrodotor8@gmail.com>
2021-01-15 14:36:54 -0500

    webpack,babel,scss ETC

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path'); //importing path module
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		app: path.resolve(__dirname, 'src/index.js'),
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+	},
+	devServer: {
+		port: 8080,
+		contentBase: path.resolve(__dirname, 'dist'),
+		//open: true,
+		//hot: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/, //Any file that end with .js
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.scss$/, //Any file that end with .scss
+				use: [
+					// Creates `style` nodes from JS strings
+					//'style-loader',
+					{
+						loader: MiniCSSExtractPlugin.loader,
+					},
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 90000,
+					},
+				},
+			},
+			{
+				test: /\.(jpg|png|gif|woff|eot|ttf|svg|mp4)$/i,
+				loader: 'file-loader',
+			},
+		],
+	},
+	plugins: [
+		//new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Plugins',
+			template: path.resolve(__dirname, 'src/index.html'),
+		}),
+		new MiniCSSExtractPlugin({
+			filename: 'styles/[name].css',
+		}),
+		new CopyWebpackPlugin({
+			patterns: [{ from: 'src/assets', to: 'assets' }],
+		}),
+	],
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+			minSize: 0,
+			name: 'commons',
+		},
+	},
+};
